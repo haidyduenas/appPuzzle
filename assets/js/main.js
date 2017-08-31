ab0b6653270b37ea4a4692b3141b400c9d82f1ab
haidy <haidy>
2017-08-31 06:24:40 -0500

    corrigiendo errores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,19 @@
 /* Crear las tarjetas con jQuery*/
 
-//var Tarjetas = '#espacioPuzzle';
 var contImagenes = [
 		"assets/img/imgPuzzle/img1.png",
-		"assets/img/imgPuzzle/img1.png",
-		"assets/img/imgPuzzle/img2.png",
 		"assets/img/imgPuzzle/img2.png",
 		"assets/img/imgPuzzle/img3.png",
-		"assets/img/imgPuzzle/img3.png",
 		"assets/img/imgPuzzle/img4.png",
-		"assets/img/imgPuzzle/img4.png",
-		"assets/img/imgPuzzle/img5.png",
 		"assets/img/imgPuzzle/img5.png",
 		"assets/img/imgPuzzle/img6.png",		
-		"assets/img/imgPuzzle/img6.png"
 		];
+
 $(function() {
 
-for (var j = 1; j< 3 ; j++) {
+for (var j = 0; j< 2 ; j++) {
 	$.each(contImagenes, function(i, val) {
-		$('#espacioPuzzle').append("<div id=tarjetas" + j + i + "><img src=" + val + " wi/>");
+		$('#espacioPuzzle').append("<div id=tarjetas" + j + i + "><img src=" + val + " />");
 	});
 }
 
